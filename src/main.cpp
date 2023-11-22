fe1db81ecc564726ef77530aa00ae283a924af9b
emneo <151401415+sademneo@users.noreply.github.com>
2023-11-22 17:34:57 +0100

    Add basic C++ project structure and D++ related stuff

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "main.hpp"
+
+int MAIN() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ src/main.hpp
@@ -0,0 +1,10 @@
+#ifndef A0E7ECCF_F345_47F2_8503_5AB691077D1A
+#define A0E7ECCF_F345_47F2_8503_5AB691077D1A
+
+#ifdef UNIT_TESTS
+#define MAIN definitely_not_main
+#else
+#define MAIN main
+#endif
+
+#endif /* A0E7ECCF_F345_47F2_8503_5AB691077D1A */
